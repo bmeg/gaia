0bf8c117c0d7bf83b6d56a9709c06c43378b17c7
Ryan Spangler <ryan.spangler@gmail.com>
2016-06-01 20:31:48 -0700

    working on nested data structures

--- server/src/main/scala/bmeg/gaea/facet/Gene.scala
+++ server/src/main/scala/bmeg/gaea/facet/Gene.scala
@@ -12,6 +12,7 @@ import org.http4s.dsl._
 
 import com.thinkaurelius.titan.core.TitanGraph
 import gremlin.scala._
+import org.apache.tinkerpop.gremlin.process.traversal.Order
 
 import com.typesafe.scalalogging._
 import _root_.argonaut._, Argonaut._
@@ -33,16 +34,24 @@ object GeneFacet extends LazyLogging {
     graph.tx.commit()
   })
 
-  def coefficientsToJson(coefficients: Map[String, Double]): Json = {
+  def coefficientsToJson(coefficients: Map[String, Double]) (key: String) (value: String): Json = {
     coefficients.foldLeft(jEmptyArray) { (json, coefficient) =>
       val (feature, level) = coefficient
-      val pair = ("feature", jString(feature)) ->: ("weight", jNumber(level).getOrElse(jZero)) ->: jEmptyObject
+      val pair = (key, jString(feature)) ->: (value, jNumber(level).getOrElse(jZero)) ->: jEmptyObject
+      pair -->>: json
+    }
+  }
+
+  def propertiesToJson(properties: Map[String, Any]) (key: String) (value: String): Json = {
+    properties.foldLeft(jEmptyArray) { (json, property) =>
+      val (name, attribute) = property
+      val pair = (key, jString(name)) ->: (value, jString(attribute.toString)) ->: jEmptyObject
       pair -->>: json
     }
   }
 
   def eventMetadata(eventID: String, eventType: String, datatype: String, weights: Map[String, Double]): Json = {
-    val weightsJson = coefficientsToJson(weights)
+    val weightsJson = coefficientsToJson(weights) ("feature") ("weight")
     ("eventID", jString(eventID)) ->: ("eventType", jString(eventType)) ->: ("datatype", jString(datatype)) ->: ("featureWeights", weightsJson) ->: jEmptyObject
   }
 
@@ -53,6 +62,21 @@ object GeneFacet extends LazyLogging {
     val signatureName = vertex.property(Name).orElse("no name")
     val metadata = eventMetadata(signatureName, "drug sensitivity signature", "NUMERIC", relevant)
     ("score", jNumber(score).getOrElse(jZero)) ->: ("signatureMetadata", metadata) ->: jEmptyObject
+  }
+
+  def expressionEvent(expressionVertex: Vertex) (level: Double) (geneNames: List[String]): Json = {
+    val metadata = eventMetadata(expressionVertex.property(Name).orElse(""), "expression values", "NUMERIC", Map[String, Double]())
+    val coefficients = SignatureWorker.dehydrateCoefficients(expressionVertex.property(Coefficients).orElse(""))
+    val relevant = SignatureWorker.selectKeys[String, Double](coefficients) (geneNames) (0.0) + ("signatureScore" -> level)
+    val sampleJson = coefficientsToJson(relevant) ("sampleID") ("value")
+    ("metadata", metadata) ->: ("sampleData", sampleJson) ->: jEmptyObject
+  }
+
+  def individualEvent(individualVertex: Vertex) (clinicalNames: List[String]): Json = {
+    val metadata = eventMetadata(individualVertex.property(Name).orElse(""), "clinical values", "STRING", Map[String, Double]())
+    val relevant = SignatureWorker.selectKeys[String, Any](individualVertex.valueMap()) (clinicalNames) ("null")
+    val clinicalJson = propertiesToJson(relevant) ("sampleID") ("value")
+    ("metadata", metadata) ->: ("sampleData", clinicalJson) ->: jEmptyObject
   }
 
   val service = HttpService {
@@ -81,7 +105,37 @@ object GeneFacet extends LazyLogging {
         val signatureMetadata = metadata("signatureMetadata")
         val expressionMetadata = metadata("expressionMetadata")
         val clinicalEventMetadata = metadata("clinicalEventMetadata")
-        Ok(json)
+
+        val signatureNames = signatureMetadata.map(_("eventID"))
+        val expressionNames = expressionMetadata.map(_("eventID"))
+        val clinicalNames = clinicalEventMetadata.map(_("eventID"))
+
+        val signatureVertexes = signatureNames.flatMap(graph.V.hasLabel("linearSignature").has(Name, _).headOption)
+        val signatureLevels = signatureVertexes.map { (signatureVertex) =>
+          val expressionEdges = signatureVertex.outE("appliesTo").orderBy("level", Order.decr).limit(50)
+          val expressionVertexes = expressionEdges.toList.map(_.in.head)
+          val individualVertexes = expressionVertexes.map(_.out("expressionFor").out("sampleOf").head)
+          (signatureVertex, expressionEdges, expressionVertexes, individualVertexes)
+        }
+
+        val response = signatureLevels.foldLeft(jEmptyArray) { (json, signature) =>
+          val (signatureVertex, expressionEdges, expressionVertexes, individualVertexes) = signature
+          val signatureName = signatureVertex.property(Name).orElse("")
+          val levels = expressionEdges.map(_.value("level").headOption.getOrElse(0.0))
+
+          val expressionEvents = expressionVertexes.zip(levels).zip(individualVertexes).map { sample =>
+            val (expression, individualVertex) = sample
+            val (expressionVertex, level) = expression
+            val eEvent = expressionEvent(expressionVertex) (level) (expressionNames)
+            val iEvent = individualEvent(individualVertex) (clinicalNames)
+            ("expressionData", eEvent) ->: ("clinicalData", iEvent) ->: jEmptyObject
+          }.asJson
+
+          val signatureEvent = ("signatureName", signatureName) ->: ("topExpressions", expressionEvents) ->: jEmptyObject
+          signatureEvent -->>: json
+        }
+
+        Ok(response)
       }
 
     case request @ POST -> Root / "gaea" / "message" / messageType =>
