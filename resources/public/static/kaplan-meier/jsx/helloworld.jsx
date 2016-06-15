e12ec9ff532867f62d3ce617c3844248da035b65
Ryan Spangler <ryan.spangler@gmail.com>
2016-06-15 14:28:38 -0700

    ubuntu script improvements

--- resources/public/static/kaplan-meier/jsx/helloworld.jsx
+++ resources/public/static/kaplan-meier/jsx/helloworld.jsx
@@ -1,184 +1,184 @@
 var identity = function(x) {return x;};
 
 var get = function(key) {
-    return function(o) {
-        return o[key];
-    }
+  return function(o) {
+    return o[key];
+  }
 }
 
 var colors = [
-    "rgb(200, 100, 0)",
-    "rgb(100, 0, 200)",
-    "rgb(0, 200, 100)"
+  "rgb(200, 100, 0)",
+  "rgb(100, 0, 200)",
+  "rgb(0, 200, 100)"
 ];
 
 var findMaxDays = function(cohort) {
-    var days = cohort.individuals.map(function(individual) {return individual.days || 0})
-    return Math.max.apply(null, days)
+  var days = cohort.individuals.map(function(individual) {return individual.days || 0})
+  return Math.max.apply(null, days)
 }
 
 var findGlobalMaxDays = function(cohorts) {
-    return Math.max.apply(null, cohorts.map(findMaxDays))
+  return Math.max.apply(null, cohorts.map(findMaxDays))
 }
 
 var KaplanMeierLine = React.createClass({
-    render: function() {
-        var line = this;
+  render: function() {
+    var line = this;
 
-        var createFlatSegment = function(interval) {
-            var xPrevious = interval.previous * line.props.dayUnits;
-            var yPrevious = line.props.height - interval.remaining * line.portionUnits;
-            var xDays = interval.days * line.props.dayUnits;
+    var createFlatSegment = function(interval) {
+      var xPrevious = interval.previous * line.props.dayUnits;
+      var yPrevious = line.props.height - interval.remaining * line.portionUnits;
+      var xDays = interval.days * line.props.dayUnits;
 
-            return (
-                    <line x1={xPrevious} y1={yPrevious} x2={xDays} y2={yPrevious} stroke={interval.color} strokeWidth="3" />
-            );
-        }
-
-        var createDropSegment = function(interval) {
-            var xDays = interval.days * line.props.dayUnits;
-            var yPrevious = line.props.height - interval.remaining * line.portionUnits;
-            var yDrop = line.props.height - (interval.remaining - 1) * line.portionUnits;
-
-            return (
-                    <line x1={xDays} y1={yPrevious} x2={xDays} y2={yDrop} stroke={interval.color} strokeWidth="3" />
-            );
-        }
-
-        var createSegment = function(interval) {
-            return (
-                    <g key={interval.key}>
-                    {createFlatSegment(interval)}
-                    {createDropSegment(interval)}
-                    </g>
-            );
-        }
-
-        var expressLosses = function(cohort) {
-            var losses = cohort.individuals.filter(get("days"));
-            var sorted = losses.sort(function(a, b) {return a.days - b.days});
-            var intervals = sorted.reduce(function(intervals, individual) {
-                var lastInterval = intervals[intervals.length - 1];
-                var previous = lastInterval ? lastInterval.days : 0;
-                var remaining = lastInterval? lastInterval.remaining - 1 : cohort.individuals.length;
-                var interval = {
-                    key: individual.name,
-                    days: individual.days,
-                    previous: previous,
-                    remaining: remaining,
-                    color: cohort.color
-                };
-
-                intervals.push(interval);
-                return intervals;
-            }, []);
-
-            return intervals;
-        }
-
-        var cohortSize = this.props.cohort.individuals.length;
-        this.portionUnits = this.props.height / cohortSize;
-        var lossEvents = expressLosses(this.props.cohort);
-        var lastEvent = lossEvents[lossEvents.length -1];
-        var finalInterval = {
-            key: "_final_",
-            days: this.props.lastDay,
-            previous: lastEvent.days,
-            remaining: lastEvent.remaining - 1,
-            color: this.props.cohort.color
-        };
-        var finalSegment = createFlatSegment(finalInterval);
-
-        return (
-                <g>
-                  {lossEvents.map(createSegment)}
-                  {finalSegment}
-                </g>
-        )
+      return (
+          <line x1={xPrevious} y1={yPrevious} x2={xDays} y2={yPrevious} stroke={interval.color} strokeWidth="3" />
+      );
     }
+
+    var createDropSegment = function(interval) {
+      var xDays = interval.days * line.props.dayUnits;
+      var yPrevious = line.props.height - interval.remaining * line.portionUnits;
+      var yDrop = line.props.height - (interval.remaining - 1) * line.portionUnits;
+
+      return (
+          <line x1={xDays} y1={yPrevious} x2={xDays} y2={yDrop} stroke={interval.color} strokeWidth="3" />
+      );
+    }
+
+    var createSegment = function(interval) {
+      return (
+          <g key={interval.key}>
+            {createFlatSegment(interval)}
+            {createDropSegment(interval)}
+          </g>
+      );
+    }
+
+    var expressLosses = function(cohort) {
+      var losses = cohort.individuals.filter(get("days"));
+      var sorted = losses.sort(function(a, b) {return a.days - b.days});
+      var intervals = sorted.reduce(function(intervals, individual) {
+        var lastInterval = intervals[intervals.length - 1];
+        var previous = lastInterval ? lastInterval.days : 0;
+        var remaining = lastInterval? lastInterval.remaining - 1 : cohort.individuals.length;
+        var interval = {
+          key: individual.name,
+          days: individual.days,
+          previous: previous,
+          remaining: remaining,
+          color: cohort.color
+        };
+
+        intervals.push(interval);
+        return intervals;
+      }, []);
+
+      return intervals;
+    }
+
+    var cohortSize = this.props.cohort.individuals.length;
+    this.portionUnits = this.props.height / cohortSize;
+    var lossEvents = expressLosses(this.props.cohort);
+    var lastEvent = lossEvents[lossEvents.length -1];
+    var finalInterval = {
+      key: "_final_",
+      days: this.props.lastDay,
+      previous: lastEvent.days,
+      remaining: lastEvent.remaining - 1,
+      color: this.props.cohort.color
+    };
+    var finalSegment = createFlatSegment(finalInterval);
+
+    return (
+        <g>
+          {lossEvents.map(createSegment)}
+          {finalSegment}
+        </g>
+    )
+  }
 });
 
 var KaplanMeierPlot = React.createClass({
-    getInitialState: function() {
-        var width = 1000;
-        var height = 1000;
-        var tumors = ["Pancreas", "Breast", "Prostate"];
-        var cohorts = {};
-        var survival = [];
-        
-        return {
-            width: width,
-            height: height,
-            tumors: tumors,
-            cohorts: cohorts,
-            survival: survival,
-            dayUnits: 1,
-            lastDay: 1
-        };
-    },
+  getInitialState: function() {
+    var width = 1000;
+    var height = 1000;
+    var tumors = ["Pancreas", "Breast", "Prostate"];
+    var cohorts = {};
+    var survival = [];
+    
+    return {
+      width: width,
+      height: height,
+      tumors: tumors,
+      cohorts: cohorts,
+      survival: survival,
+      dayUnits: 1,
+      lastDay: 1
+    };
+  },
 
-    loadSurvival: function(tumor, cohort) {
-        $.ajax({
-            url: this.props.survivalURL,
-            dataType: 'json',
-            type: 'POST',
-            data: JSON.stringify(cohort),
-            success: function(survival) {
-                this.state.survival.push({name: tumor, individuals: survival, color: colors[this.state.survival.length]})
-                var maxDays = findGlobalMaxDays(this.state.survival);
-                var lastDay = Math.floor(maxDays * 1.2);
-                var dayUnits = this.state.width / lastDay;
+  loadSurvival: function(tumor, cohort) {
+    $.ajax({
+      url: this.props.survivalURL,
+      dataType: 'json',
+      type: 'POST',
+      data: JSON.stringify(cohort),
+      success: function(survival) {
+        this.state.survival.push({name: tumor, individuals: survival, color: colors[this.state.survival.length]})
+        var maxDays = findGlobalMaxDays(this.state.survival);
+        var lastDay = Math.floor(maxDays * 1.2);
+        var dayUnits = this.state.width / lastDay;
 
-                this.setState({
-                    survival: this.state.survival,
-                    lastDay: lastDay,
-                    dayUnits: dayUnits
-                });
-            }.bind(this),
-            error: function(xhr, status, err) {
-                console.error(this.props.survivalURL, status, err.toString());
-            }.bind(this)
+        this.setState({
+          survival: this.state.survival,
+          lastDay: lastDay,
+          dayUnits: dayUnits
         });
-    },
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.survivalURL, status, err.toString());
+      }.bind(this)
+    });
+  },
 
-    loadCohort: function(tumor) {
-        var url = this.props.cohortURL + tumor;
-        $.ajax({
-            url: url,
-            dataType: 'json',
-            type: 'GET',
-            success: function(cohort) {
-                this.state.cohorts[tumor] = cohort
-                this.setState({cohorts: this.state.cohorts});
-                this.loadSurvival(tumor, cohort);
-            }.bind(this),
-            error: function(xhr, status, err) {
-                console.error(url, status, err.toString());
-            }.bind(this)
-        });
-    },
+  loadCohort: function(tumor) {
+    var url = this.props.cohortURL + tumor;
+    $.ajax({
+      url: url,
+      dataType: 'json',
+      type: 'GET',
+      success: function(cohort) {
+        this.state.cohorts[tumor] = cohort
+        this.setState({cohorts: this.state.cohorts});
+        this.loadSurvival(tumor, cohort);
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(url, status, err.toString());
+      }.bind(this)
+    });
+  },
 
-    componentDidMount: function() {
-        for (var t = 0; t < this.state.tumors.length; t++) {
-            this.loadCohort(this.state.tumors[t]);
-        }
-    },
-
-    render: function() {
-        var plot = this;
-
-        var createLine = function(cohort) {
-            return <KaplanMeierLine key={cohort.name} cohort={cohort} width={plot.state.width} height={plot.state.height} dayUnits={plot.state.dayUnits} lastDay={plot.state.lastDay} />
-        }
-
-        return (
-            <div>
-              <svg width={this.state.width} height={this.state.height}>
-                {this.state.survival.map(createLine)}
-              </svg>
-            </div>
-        );
+  componentDidMount: function() {
+    for (var t = 0; t < this.state.tumors.length; t++) {
+      this.loadCohort(this.state.tumors[t]);
     }
+  },
+
+  render: function() {
+    var plot = this;
+
+    var createLine = function(cohort) {
+      return <KaplanMeierLine key={cohort.name} cohort={cohort} width={plot.state.width} height={plot.state.height} dayUnits={plot.state.dayUnits} lastDay={plot.state.lastDay} />
+    }
+
+    return (
+        <div>
+          <svg width={this.state.width} height={this.state.height}>
+            {this.state.survival.map(createLine)}
+          </svg>
+        </div>
+    );
+  }
 });
 
 ReactDOM.render(<KaplanMeierPlot cohortURL="/gaea/individual/tumor/" survivalURL="/gaea/individual/survival" />, document.getElementById('kaplan-meier'));
--- script/ubuntu.sh
+++ script/ubuntu.sh
@@ -5,8 +5,9 @@
 # INSTALLING GAEA ------------------------------------
 
 # install jdk
-sudo apt-get install openjdk-8-jdk
-sudo apt-get install zip unzip ruby
+sudo apt-get update
+sudo apt-get --force-yes install openjdk-8-jdk
+sudo apt-get --force-yes install zip unzip ruby
 
 # install maven
 mkdir -p ~/bin && curl -s https://bitbucket.org/mjensen/mvnvm/raw/master/mvn > ~/bin/mvn && chmod 0755 ~/bin/mvn
@@ -15,8 +16,13 @@ export PATH=~/bin:$PATH
 # install sbt
 echo "deb https://dl.bintray.com/sbt/debian /" | sudo tee -a /etc/apt/sources.list.d/sbt.list
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 642AC823
-sudo apt-get update
-sudo apt-get install sbt
+sudo apt-get --force-yes install sbt
+
+# configure git
+git config --global alias.co checkout
+git config --global alias.ci commit
+git config --global alias.st status
+git config --global alias.br branch
 
 # install titan
 git clone https://github.com/thinkaurelius/titan.git
@@ -28,7 +34,6 @@ cd ..
 
 # install gaea
 git clone https://github.com/bmeg/gaea.git
-
 
 # SBT CONFIGURATION -----------------------------------------
 
