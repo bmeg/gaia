9954a9a6b8aa636b6831a4ab73947b2ebead503f
Ryan Spangler <ryan.spangler@gmail.com>
2016-06-29 17:16:22 -0700

    pie chart showing

--- resources/public/static/cohort/js/cohort.js
+++ resources/public/static/cohort/js/cohort.js
@@ -1,23 +1,39 @@
-var cohort = [
-  {'tumor': 'Pancreas', 'population': 12},
-  {'tumor': 'Breast', 'population': 22},
-  {'tumor': 'Prostate', 'population': 32},
-];
+function drawPie() {
 
-var pie = d3.layout.pie().value(function(d) {return d.population});
-var slices = pie(cohort);
+  var cohort = [
+    {'tumor': 'Pancreas', 'population': 12},
+    {'tumor': 'Breast', 'population': 22},
+    {'tumor': 'Prostate', 'population': 32},
+  ];
 
-var arc = d3.svg.arc().innerRadius(0).outerRadius(100);
-var color = d3.scale.category10();
+  var pie = d3.layout.pie().value(function(d) {return d.population});
+  var slices = pie(cohort);
 
-var svg = d3.select('svg.cohort');
-var g = svg.append('g').attr('transform', 'translate(200, 200)');
+  var arc = d3.svg.arc().innerRadius(0).outerRadius(100);
+  var color = d3.scale.category10();
 
-g.selectAll('path.slice')
-  .data(slices)
-  .enter()
-  .append('path')
-  .attr('class', 'slice')
-  .attr('d', arc)
-  .attr('fill', function(d) {return color(d.data.tumor)});
+  var svg = d3.select('svg.cohort');
+  var g = svg.append('g').attr('transform', 'translate(200, 200)');
+
+  console.log(svg);
+  console.log(slices);
+
+  g.selectAll('path.slice')
+    .data(slices)
+    .enter()
+    .append('path')
+    .attr('class', 'slice')
+    .attr('d', arc)
+    .attr('fill', function(d) {return color(d.data.tumor)});
+
+  svg.append('g')
+    .attr('class', 'legend')
+    .selectAll('text')
+    .data(slices)
+    .enter()
+    .append('text')
+    .text(function(d) { return '• ' + d.data.tumor; })
+    .attr('fill', function(d) { return color(d.data.tumor); })
+    .attr('y', function(d, i) { return 20 * (i + 1); })
+}
 
