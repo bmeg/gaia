ea1fd54e419ea10686422ffe60f7421b56f4429a
Ryan Spangler <ryan.spangler@gmail.com>
2016-07-29 15:38:25 -0700

    additional notes for kafka setup

--- script/ubuntu.sh
+++ script/ubuntu.sh
@@ -65,6 +65,71 @@ broadcast_rpc_address: 10.104.0.5
 
 
 
+# INSTALLING DOCKER ---------------------------------------
+
+sudo apt-get install apt-transport-https ca-certificates
+sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+sudo echo "deb https://apt.dockerproject.org/repo ubuntu-vivid main" > /etc/apt/sources.list.d/docker.list
+sudo apt-get update
+apt-cache policy docker-engine
+sudo apt-get install docker-engine
+sudo service docker start
+
+
+
+# INSTALLING KAFKA ----------------------------------------------
+
+wget http://mirror.symnds.com/software/Apache/kafka/0.10.0.0/kafka_2.11-0.10.0.0.tgz
+tar xzvf kafka_2.11-0.10.0.0.tgz
+cd kafka_2.11-0.10.0.0
+tmux
+bin/zookeeper-server-start.sh config/zookeeper.properties
+C-b c
+bin/kafka-server-start.sh config/server.properties
+C-b c
+bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic test
+bin/kafka-topics.sh --list --zookeeper localhost:2181
+bin/kafka-console-producer.sh --broker-list localhost:9092 --topic test
+C-b c
+bin/kafka-console-consumer.sh --zookeeper localhost:2181 --topic test --from-beginning
+
+# example zookeeper config ********
+
+# the directory where the snapshot is stored.
+dataDir=/var/zookeeper/data
+# the port at which the clients will connect
+clientPort=2181
+# disable the per-ip limit on the number of connections since this is a non-production config
+maxClientCnxns=0
+server.1=10.40.40.24:2888:3888
+server.2=10.40.40.23:2888:3888
+server.3=10.40.40.25:2888:3888
+initLimit=5
+syncLimit=2
+
+# example kafka config *********
+
+broker.id=1
+advertised.listeners=PLAINTEXT://10.96.11.91:9092
+zookeeper.connect=10.40.40.23:2181,10.40.40.24:2181,10.40.40.25:2181
+
+
+
+# EXASTACK CONFIG ---------------------------------------------
+
+#cloud-config
+users:
+  - name: spanglry
+    gecos: Ryan Spangler
+    sudo: ['ALL=(ALL) NOPASSWD:ALL']
+    shell: /bin/bash
+    groups: sudo
+    ssh-authorized-keys:
+      - ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDGndX51JpObA9/MC+p60s/9S54j0OjqlO7f/Tz9U2GsnDmMki8XpZOw0klJG7YFniIWhLKq1caAEk1/OyLjB8693h2Rv7HAdnNnUba5YmnsI9sgdHqZ5iMSD5qPNQDcmepvPt0k/UO0PzuSKeO4CmnWfZUIy0pTvWrP6Q4rXQzT6VlcxxlAFlwPNmZ6k0Oezf9QhYFBBHU3m0fOMbLFoiSv+k833TuawZYoM/TcIfwVo5lhiJqb6+mguTvjKkvIr1a6zJrTOK6lkPoZaTnME4Nr3P6rwdl5PbjOlS8g1tBHOarHmScuOmfnLlzQi+DRf10ZPBQW/ncppaON79wleVpUYuccKv6dffIeZQ3oqvGKJxLXRzE5gy5Zy8DEXsYapMwCU+KlVkHTDnSWnwznVG8jDs1KdWBj2hLZu3L3xNAYSpEbn8NrcY1bp1s8KziFN24efxbJmABnINYguMNajc/lCHwRJC1dGE/keYxuGgvpSuQUB8+XkpW7wv7u7GoTQ0yt7BxyHttKmuowOMf44N+cYjpQIC0ftO6Xet65UqCH+VpQ/AbHWIsdYsl3gISGYoE3rUYmZsuYed/l6V4CndAqpA5GBeQJeK+Jp61vFQTJ94uXQHObQjU8HeUTvqPc4kkzTQQ/2Jx0/HLq0Us/6EPrkfE3wOLFvaO/ByYoeT9DQ== ryan.spangler@gmail.com
+
+package_upgrade: true
+# end of file
+
 
 
 
