7bc0a94df46d3847fbefa91f4f11f8b08a760d6b
Ryan Spangler <ryan.spangler@gmail.com>
2016-08-24 11:02:34 -0700

    bumping timeout

--- server/src/main/scala/gaea/Server.scala
+++ server/src/main/scala/gaea/Server.scala
@@ -1,10 +1,13 @@
 package gaea
 
 import gaea.facet.GeneFacet
+import scala.concurrent.duration._
 import org.http4s.server.blaze.BlazeBuilder
 
 object GaeaFoundation extends App {
-  BlazeBuilder.bindHttp(11223)
+  BlazeBuilder
+    .withIdleTimeout(60.seconds)
+    .bindHttp(11223)
     .mountService(GeneFacet.service, "/")
     .run
     .awaitShutdown()
