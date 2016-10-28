eb5c30d9bbc229f708687116b501a7c2afacb916
Kyle Ellrott <kellrott@gmail.com>
2016-10-28 12:05:44 -0700

    Script dir cleanup and adding kafak utility script

--- script/single-machine-stop.sh
+++ script/single-machine-stop.sh
@@ -4,7 +4,7 @@ KAFKA_VERSION=0.10.0.1
 CASSANDRA_VERSION=3.7
 APACHE_MIRROR=www.gtlib.gatech.edu
 
-BDIR="$(cd `dirname $0`; pwd)"
+BDIR="$(cd `dirname $0 && cd ../`; pwd)"
 cd $BDIR
 
 ./kafka_2.11-$KAFKA_VERSION/bin/kafka-server-stop.sh 
