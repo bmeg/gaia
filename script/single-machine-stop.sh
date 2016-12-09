8d64ebe85237cc5ba4b7b666fc4433cf23e8d4e6
Kyle Ellrott <kellrott@gmail.com>
2016-12-09 12:07:21 -0800

    Working on kafka deployment and utility scripts

--- script/single-machine-stop.sh
+++ script/single-machine-stop.sh
@@ -4,9 +4,8 @@ KAFKA_VERSION=0.10.0.1
 CASSANDRA_VERSION=3.7
 APACHE_MIRROR=www.gtlib.gatech.edu
 
-BDIR="$(cd `dirname $0 && cd ../`; pwd)"
-cd $BDIR
+BDIR="$(cd `dirname $0` && cd ../ && pwd)"
 
-./kafka_2.11-$KAFKA_VERSION/bin/kafka-server-stop.sh 
-./kafka_2.11-$KAFKA_VERSION/bin/zookeeper-server-stop.sh 
-kill `cat ./apache-cassandra-$CASSANDRA_VERSION/cassandra.pid`
+$BDIR/kafka_2.11-$KAFKA_VERSION/bin/kafka-server-stop.sh
+$BDIR/kafka_2.11-$KAFKA_VERSION/bin/zookeeper-server-stop.sh
+kill `cat $BDIR/apache-cassandra-$CASSANDRA_VERSION/cassandra.pid`
