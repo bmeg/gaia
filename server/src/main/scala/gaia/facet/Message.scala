38d7533d2a67b6b68b2df1f71d03dfc804d26240
Ryan Spangler <ryan.spangler@gmail.com>
2017-03-14 15:17:16 -0700

    added endpoint for streaming ingestion, improved ui, new todosss

--- server/src/main/scala/gaia/facet/Message.scala
+++ server/src/main/scala/gaia/facet/Message.scala
@@ -37,9 +37,24 @@ case class MessageFacet(root: String) extends GaiaFacet with LazyLogging {
     transform.transform(map)
   }
 
+  def ingestVertex(transform: GraphTransform) (label: String) (line: String): Task[Vertex] = Task {
+    val map = JsonIO.readMap(line)
+    transform.ingestVertex(label) (map)
+  }
+
   def service(graph: GaiaGraph): HttpService = {
     val transform = GraphTransform(graph)
     HttpService {
+      case request @ POST -> Root / "ingest" / label =>
+        val ingest = ingestVertex(transform) (label) _
+        val vertexes = request.body.pipe(text.utf8Decode).pipe(text.lines(1024 * 1024)).flatMap { line =>
+          print(".")
+          Process.eval(ingest(line))
+        }
+
+        vertexes.runLog.run
+        Ok("done!")
+
       case request @ POST -> Root / "ingest" =>
         val messages = request.bodyAsText.pipe(text.lines(1024 * 1024 * 64)).flatMap { line =>
           Process eval transformMessage(transform) (line)
