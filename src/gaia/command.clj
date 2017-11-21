6d7be24cae6ec08b11350690b8465a276318e504
Ryan Spangler <ryan.spangler@gmail.com>
2017-11-21 14:41:12 -0800

    generating processes from applying composite command

--- src/gaia/command.clj
+++ src/gaia/command.clj
@@ -11,13 +11,13 @@
   (set/difference (set a) (set b)))
 
 (defn validate-apply-composite!
-  [{:keys inputs outputs} process]
+  [{:keys [inputs outputs]} process]
   (let [pin (:inputs process)
         pout (:outputs process)
         pvars (:vars process)]
-    (if-not (and (empty? (difference inputs (keys pin)))
-                 (empty? (difference outputs (keys pout))))
-      (throw (Exception. (str (:key process) " - all inputs and outputs must be specified"))))))
+    (if-not (and (empty? (difference (map keyword inputs) (keys pin)))
+                 (empty? (difference (map keyword outputs) (keys pout))))
+      (throw (Exception. (str (:key process) " - all inputs and outputs must be specified: " inputs outputs))))))
 
 (defn substitute-values
   [template values]
@@ -31,6 +31,7 @@
 (defn generate-binding
   [key output global]
   (str
+   "generated/"
    key "-"
    output "-"
    global "-"
@@ -40,10 +41,8 @@
   [process template outputs]
   (reduce
    (fn [result [k t]]
-     (if-let [value (get outputs (keyword t))]
-       (assoc-in result [:final k] value)
-       (assoc-in result [:inner t] (generate-binding (:key process) k t))))
-   {:final {} :inner {}} template))
+     (assoc result k (get outputs (keyword t))))
+   {} template))
 
 (defn generate-variables
   "we need some way to find out what the keys of the generated inputs are
@@ -54,21 +53,31 @@
   [process vars inputs outputs {:keys [key command] :as step}]
   (let [ovars (substitute-values (:vars step) vars)
         oin (substitute-values (:inputs step) inputs)
-        {:keys [final inner]} (apply-outputs process (:outputs step) outputs)]
-    {:inner inner
-     :step
-     {:key key
-      :command command
-      :vars ovars
-      :inputs oin
-      :outputs (merge inner final)}}))
+        oout (substitute-values (:outputs step) outputs)]
+        ;; oout (apply-outputs process (:outputs step) outputs)
+    {:key key
+     :command command
+     :vars ovars
+     :inputs oin
+     :outputs oout}))
+
+(defn generate-outputs
+  [process outputs step]
+  (reduce
+   (fn [all [key template]]
+     (if (get-in process [:outputs (keyword template)])
+       all
+       (assoc all (keyword template) (generate-binding (:key process) (name key) template))))
+   outputs (:outputs step)))
 
 (defn apply-composite
-  [flow {:keys [vars inputs outputs steps] :as command} process]
+  [{:keys [vars inputs outputs steps] :as command} process]
   (validate-apply-composite! command process)
   (let [pin (:inputs process)
         pout (:outputs process)
         pvars (:vars process)
         pkey (:key process)
-        asteps (map (partial apply-steps process pvars pin pout) steps)]))
+        generated (reduce (partial generate-outputs process) {} steps)
+        asteps (map (partial apply-step process pvars (merge pin generated) (merge pout generated)) steps)]
+    asteps))
 
