1b2afdff2a728c5597f679792095b8b64bbe7ec3
Ryan Spangler <ryan.spangler@gmail.com>
2017-12-13 23:27:56 -0800

    created service for status and expire

--- src/gaia/core.clj
+++ src/gaia/core.clj
@@ -2,6 +2,11 @@
   (:require
    [taoensso.timbre :as log]
    [aleph.http :as http]
+   [ring.middleware.resource :as resource]
+   [ring.middleware.params :as params]
+   [ring.middleware.keyword-params :as keyword]
+   [cheshire.core :as json]
+   [polaris.core :as polaris]
    [protograph.kafka :as kafka]
    [ophion.config :as config]
    [gaia.config :as gaia]
@@ -10,11 +15,23 @@
    [gaia.flow :as flow]
    [gaia.funnel :as funnel]
    [gaia.trigger :as trigger]
-   [gaia.sync :as sync]))
+   [gaia.sync :as sync])
+  (:import
+   [java.io InputStreamReader]))
 
 (defn pp
   [clj]
   (with-out-str (clojure.pprint/pprint clj)))
+
+(defn read-json
+  [body]
+  (json/parse-stream (InputStreamReader. body) keyword))
+
+(defn response
+  [body]
+  {:status 200
+   :headers {"content-type" "application/json"}
+   :body (json/generate-string body)})
 
 (defn load-config
   [path]
@@ -37,28 +54,55 @@
     (log/info "funnel config" funnel-config)
     (funnel/funnel-connect funnel-config (:gaia config))))
 
+(defn status-handler
+  [flow]
+  (fn [request]
+    (let [{:keys [key] :as body} (read-json (:body request))]
+      (log/info "status request" body)
+      (response
+       {:key key
+        :status (get @(:status flow) key)}))))
+
+(defn expire-handler
+  [flow]
+  (fn [request]
+    (let [{:keys [key] :as body} (read-json (:body request))
+          implicated (flow/find-descendants (:flow flow) key)]
+      (log/info "expire request" body)
+      (swap! (:status flow) (fn [status] (apply dissoc status implicated)))
+      (sync/trigger-election! flow)
+      (response
+       {:expired implicated}))))
+
+(defn gaia-routes
+  [flow]
+  [["/status" :status (status-handler flow)]
+   ["/expire" :expire (expire-handler flow)]])
+
 (defn boot
   [config]
   (let [store (load-store (:store config))
         funnel (boot-funnel config store)
         flow (sync/generate-sync funnel (:gaia config))
+        flow (assoc flow :store store)
         events (sync/events-listener flow (:kafka config))]
     (sync/engage-sync! flow)
-    (assoc flow :store store)))
-
-(defn app
-  [request]
-  {:status 200
-   :body "yellow"})
+    flow))
 
 (defn start
   []
   (let [;; config (load-config "config/ohsu-swift.clj")
         ;; config (load-config "config/gaia.clj")
-        config (load-config "config/home.clj")]
-    (boot config)))
+        config (load-config "config/home.clj")
+        flow (boot config)
+        routes (polaris/build-routes (gaia-routes flow))
+        router (polaris/router routes)
+        app (-> router
+                (resource/wrap-resource "public")
+                (keyword/wrap-keyword-params)
+                (params/wrap-params))]
+    (http/start-server app {:port 24442})))
 
 (defn -main
   []
-  (start)
-  (http/start-server app {:port 24442}))
+  (start))
